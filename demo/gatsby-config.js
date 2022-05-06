a0444d42b912a223e132d65b39f0db7ace567fd7
mrozo <moreez@protonmail.com>
2022-05-06 22:54:30 +0100

    Added visual layout

---
+++ demo/gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `Click and rent`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
---
+++ demo/src/pages/index.js
@@ -0,0 +1,60 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  maxWidth: 380
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 24,
+  maxWidth: "100%",
+}
+
+const emailLabel = {
+  fontWeight: "bold",
+  fontSize: 16,
+  verticalAlign: "5%",
+  width: "100%",
+}
+
+const emailInput = {
+  height: 30,
+  fontSize: 20,
+  marginTop: 5,
+  width: "96%",
+  paddingLeft: 10,
+}
+
+const emailButton = {
+  height: 36,
+  width: "100%",
+  fontSize: 17,
+  marginTop: 15,
+  border: "none",
+  backgroundColor: "orange"
+}
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      
+      <title>Click and rent</title>
+      <h1 style={headingStyles}>
+        Please enter your email to receive the code.
+      </h1>
+      <label style={emailLabel} name="email">Your email</label>
+      <br />
+      <input style={emailInput} name="email" placeholder="johndoe@gmail.com" type="email" />
+      <br />
+      <button style={emailButton}>
+        Generate
+      </button>
+    </main>
+  )
+}
+
+export default IndexPage
