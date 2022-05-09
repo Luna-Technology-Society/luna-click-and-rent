defc995d79d555c4f1bf4b4f37aa5c482975ba40
mrozo <moreez@protonmail.com>
2022-05-09 15:44:53 +0100

    set port to 80

--- demo-backend/app2.js
+++ demo-backend/app2.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const nodemailer = require("nodemailer");
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
