1a784e77a5fd021ed96dafd219741f3dabd3fdca
mrozo <moreez@protonmail.com>
2022-05-09 14:22:53 +0100

    changed port to 80

--- demo-backend/app2.js
+++ demo-backend/app2.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
