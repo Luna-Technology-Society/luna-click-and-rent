ca881362ab246d434b8c189c2bbf3b943e0e8226
mrozo <moreez@protonmail.com>
2022-05-10 13:48:58 +0100

    cors fix 3

--- demo-backend/app2.js
+++ demo-backend/app2.js
@@ -11,13 +11,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
 app.use(bodyParser.json());
-app.use(cors({ origin: 'https://clickandrent-demo.web.app/' , credentials :  true}));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    next();
-});
+app.use(cors({ origin: 'https://clickandrent-demo.web.app/'}));
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//     next();
+// });
 
 
 app.get('/', (req, res) => {
