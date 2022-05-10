72cc4b201d73a838169143fab8644da8b2e34d9d
mrozo <moreez@protonmail.com>
2022-05-10 13:46:40 +0100

    cross origin request fix

--- demo-backend/app2.js
+++ demo-backend/app2.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ origin: 'https://clickandrent-demo.web.app/' , credentials :  true}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
