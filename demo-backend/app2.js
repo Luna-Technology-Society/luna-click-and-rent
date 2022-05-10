6fb95ee6bb14078254990579a87ed0c65ef5a948
mrozo <moreez@protonmail.com>
2022-05-10 13:44:20 +0100

    cross origin request fix

--- demo-backend/app2.js
+++ demo-backend/app2.js
@@ -12,6 +12,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
 
 
 app.get('/', (req, res) => {
