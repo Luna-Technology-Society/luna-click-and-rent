088b35496a1835a5dc12a2f99b5296adfbdbc800
Oskar Mroz <moreez@protonmail.com>
2022-05-12 01:49:43 +0100

    added validation and loading screen

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from 'axios';
 import "../styles/index.css"
-import Loader from "react-loader"
+import RingLoader from "react-spinners/RingLoader";
 
 // markup
 const IndexPage = () => {
@@ -25,7 +25,7 @@ const IndexPage = () => {
       }).then((response) => {
         console.log(response.data.message);
         if (response.data.error) {
-          setResultVal("Error has occured! Check console.");
+          setResultVal("Error has occured!\n" + response.data.error.message ? response.data.error.message : "");
           console.log(response.data.error)
         } else {
           setResultVal("Successfully sent!");
@@ -33,7 +33,7 @@ const IndexPage = () => {
         }
         setIsLoaded(true);
       }).catch(err => {
-        setResultVal("Error has occured! Check console.");
+        setResultVal("Error has occured!\n" + err.message ? err.message : "");
         console.log(err);
         setIsLoaded(true);
       })
@@ -44,34 +44,60 @@ const IndexPage = () => {
   }
 
   return (
-    <main style={pageStyles}>
-      <Loader loaded={isLoaded} lines={13} length={20} width={10} radius={30}
-        corners={1} rotate={0} direction={1} color="#000" speed={1}
-        trail={60} shadow={false} hwaccel={false} className="spinner"
-        zIndex={2e9} top="50%" left="50%" scale={1.00}
-        loadedClassName="loadedContent" />
-      <title>Click and rent</title>
-      <h1 style={headingStyles}>
-        Please enter your email to receive the code.
-      </h1>
-      <label style={emailLabel} name="email">Your email</label>
-      <br />
-      <input style={emailInput} value={inputVal} onChange={handleChange} name="email" placeholder="johndoe@gmail.com" type="email" />
-      <br />
-      <button type="submit" style={emailButton} onClick={handleClick}>
-        Generate
-      </button>
-      <div style={resultOut}>{resultVal}</div>
+    <main style={mainContainer}>
+      <div style={pageStyles}>
+        {isLoaded ? (
+          <>
+            <title>Click and rent</title>
+            <h1 style={headingStyles}>
+              Please enter your email to receive the code.
+            </h1>
+            <label style={emailLabel} name="email">Your email</label>
+            <br />
+            <input style={emailInput} value={inputVal} onChange={handleChange} name="email" placeholder="johndoe@gmail.com" type="email" />
+            <br />
+            <button type="submit" style={emailButton} onClick={handleClick}>
+              Generate
+            </button>
+            <div style={resultOut}>{resultVal}</div>
+          </>
+        ) : (
+          <>
+            <RingLoader color={'black'} loading={!isLoaded} size={100} css={spinnerStyle} />
+            <h2 style={{ marginTop: 30, textAlign: "center" }}>Loading...</h2>
+          </>
+        )}
+
+      </div>
     </main>
   )
 }
 
-// styles
+const spinnerStyle = `
+display: block;
+margin: 0 auto;
+border-color: red;
+position: absolute;
+top: 50%;
+left: 50%;
+`;
+
+const mainContainer = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "100vw"
+}
+
 const pageStyles = {
   color: "#232129",
   padding: "5%",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-  maxWidth: 380
+  maxWidth: 380,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  postiion: 'relative'
 }
 const headingStyles = {
   marginTop: 0,
