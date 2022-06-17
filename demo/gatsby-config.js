e5dc747c6f9d442f58ba8505b515baac83a59d93
MoreeZ <moreez@protonmail.com>
2022-06-17 18:47:59 -0500

    added gatsby-plugin-offline and manifest

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -3,5 +3,21 @@ module.exports = {
     title: `Click and rent`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [{
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `CodeWithLinda`,
+      short_name: `CodeWithLinda`,
+      start_url: `/`,
+      background_color: `#212121`,
+      theme_color: `#f39ca9`,
+      display: `standalone`,
+      icon: `src/images/icon.png`
+    },
+  }, "gatsby-plugin-styled-components", {
+    resolve: `gatsby-plugin-offline`,
+    options: {
+      precachePages: [`/`, `/app/access/*`],
+    },
+  },]
 };
