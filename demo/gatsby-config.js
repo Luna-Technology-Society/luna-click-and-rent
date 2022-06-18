fe2ca8fb953031e60c60b11cfd7f09abd3b2b315
MoreeZ <moreez@protonmail.com>
2022-06-17 19:06:11 -0500

    updated manifest to start at link

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     options: {
       name: `Click & Rent Demo`,
       short_name: `C&R Demo`,
-      start_url: `/`,
+      start_url: `/app/access/%7B"deviceName"%3A"Door%201"%2C"entryKey"%3A1610%7D`,
       background_color: `#ffffff`,
       theme_color: `#0d96a8`,
       display: `standalone`,
